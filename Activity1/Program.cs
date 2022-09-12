5b5905f5d32c119244fbc5780c1f36b5dbf9ec19
wayneCruz <84614229+wayneCruz@users.noreply.github.com>
2022-09-13 03:16:40 +0800

    Add files via upload

---
+++ Activity1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairingArraysByIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] email = { "reg.realeza@gmail.com", "uchiha.itachi@gmail.com", "hana.uzaki@gmail.com"};
+            string[] username = { "rreginarose", "itachiuchiha", "uzakichan" };
+            string[] password = {"Yeyel", "shinobi", "sensei"};
+
+            Console.Write("Index: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Email: " + (email[index]));
+            Console.WriteLine("Username: " + username[index]);
+            Console.WriteLine("Password: " + password[index]);
+            Console.ReadKey();
+        }
+    }
+}
