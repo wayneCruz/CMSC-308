7a8f66886407978349416a2e7504b301ec02de43
wayneCruz <84614229+wayneCruz@users.noreply.github.com>
2022-10-08 17:22:30 +0800

    Add files via upload

---
+++ Activity 5/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace CMSC308_activity5
+{
+    class mainProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+
+            boardMarker marker = new boardMarker();
+
+            int choice = 0;
+            string msg = string.Empty;
+            string userColor = string.Empty;
+
+            marker.inkColor = "black";
+            marker.inkContent = 20;
+
+            while (true)
+            {
+                switch (marker.inkColor)
+                {
+                    case "blue":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case "red":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case "black":
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        break;
+                }
+
+                Console.Clear();
+                Console.WriteLine("==MENU==");
+                Console.WriteLine("1. Display Info");
+                Console.WriteLine("2. Change Color");
+                Console.WriteLine("3. Refill");
+                Console.WriteLine("4. Write");
+                Console.Write("\nEnter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Clear();
+
+                        Console.WriteLine("Ink Content: " + marker.inkContent);
+                        Console.WriteLine("Ink Color: " + marker.inkColor);
+                        Console.WriteLine("\nPress Enter to Return to MENU...");
+                        Console.ReadKey();
+                        Console.Write("\n");
+                        break;
+
+                    case 2:
+                        if (marker.inkContent != 0)
+                        {
+                            Console.Clear();
+                            marker.note();
+                            Console.WriteLine("\nPress Enter to Return to MENU...");
+
+                            Console.ReadKey();
+                        }
+
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Choose from: Black, Blue and Red");
+                            Console.Write("New Color: ");
+                            userColor = Console.ReadLine();
+                            marker.changeColor(userColor);
+                            Console.WriteLine("Ink Color = " + marker.inkColor);
+                            Console.WriteLine("\nPress Enter to Return to MENU...");
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case 3:
+
+                        Console.Clear();
+                        marker.refill();
+                        Console.WriteLine("\nPress Enter to Return to MENU...");
+                        Console.ReadKey();
+
+                        break;
+
+                    case 4:
+
+                        if (marker.inkContent == 0)
+                        {
+                            marker.noInk();
+                        }
+                        else
+                        {
+                            Console.Write("Write: ");
+                            msg = Console.ReadLine();
+                            Console.Write("\n");
+                            Console.Write("Message: ");
+                            marker.write(msg);
+                            Console.Write("\n");
+                            Console.WriteLine("\nPress Enter to Return to MENU...");
+                            Console.ReadKey();
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Activity 5/boardMarker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CMSC308_activity5
+{
+    public struct boardMarker
+    {
+        public string inkColor;
+        public int inkContent;
+
+        public void write(string msg)
+        {
+            inkContent -= 10;
+            Console.WriteLine(msg);
+            Console.WriteLine("Remaining ink: " + inkContent);
+        }
+
+        public void refill()
+        {
+            inkContent = 20;
+            Console.WriteLine("Remaining ink: " + inkContent);
+        }
+
+        public void changeColor(string userColor)
+        {
+            inkColor = userColor;
+
+            switch (inkColor)
+            {
+                case "Black":
+                    inkColor = "black";
+                    break;
+                case "Blue":
+                    inkColor = "blue";
+                    break;
+                case "Red":
+                    inkColor = "red";
+                    break;
+            }
+        }
+
+        public void note()
+        {
+            Console.WriteLine("Note: You cannot change the color if the ink is not empty");
+            Console.WriteLine("Ink Content: " + inkContent);
+        }
+
+        public void noInk()
+        {
+            Console.WriteLine("There's no more ink please Refill...");
+            Console.ReadKey();
+        }
+    }
+}
